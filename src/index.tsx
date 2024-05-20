6e878cf00ade988e71c4dd15c0cb1f3551fd2b13
Ajetunmobi Isaac <isaacajetunmobi@gmail.com>
2024-05-20 17:37:35 +0100

    exported FlexGrid & ResponsiveGrid Types

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,7 @@
 import { FlexGrid } from './flex-grid';
 import { ResponsiveGrid } from './responsive-grid';
+import type { FlexGridProps } from './flex-grid/types';
+import type { ResponsiveGridProps } from './responsive-grid/types';
 
 export { FlexGrid, ResponsiveGrid };
+export type { FlexGridProps, ResponsiveGridProps };
